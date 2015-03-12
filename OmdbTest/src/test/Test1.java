2c719a65853806b3532d4783fa96889fc6245695
bilgealas <bilgealas@gmail.com>
2015-03-12 23:07:11 +0200

    İkinci değişiklik.

--- OmdbTest/src/test/Test1.java
+++ OmdbTest/src/test/Test1.java
@@ -1,7 +1,17 @@
 package test;
 
+import java.awt.print.Printable;
+
 public class Test1 {
+	
+	String name;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+	}
+	
+	public void printWorld(String name){
+		this.name = name;
+		System.out.println("World is : " + name);
 	}
 }
