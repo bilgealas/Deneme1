9cc35d5b5868108d7d3fefabedf4f4f6ffe8aa2a
bilgealas <bilgealas@gmail.com>
2015-04-29 23:12:40 +0300

    ilk insert

--- OmdbTest/src/omdb/SqlInsert.java
+++ OmdbTest/src/omdb/SqlInsert.java
@@ -1,7 +1,7 @@
 package omdb;
 
-import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
@@ -9,112 +9,111 @@ import java.text.SimpleDateFormat;
 
 public class SqlInsert {
 
-	private static final String DB_DRIVER = "oracle.jdbc.driver.OracleThinDriver";
+	private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
 	private static final String DB_CONNECTION = "jdbc:oracle:thin:@//localhost:1521/XE";
 	private static final String DB_USER = "SYSTEM";
 	private static final String DB_PASSWORD = "bilge1510";
-	private static final DateFormat dateFormat = new SimpleDateFormat(
-			"yyyy/MM/dd HH:mm:ss");
-	
-	
-	public static void main(String[] argv) {
-		 
-		try {
- 
-			insertRecordIntoDbUserTable();
- 
-		} catch (SQLException e) {
- 
-			System.out.println(e.getMessage());
-			
-		}
- 
-		}
-		
+	private static final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-		private static <insert> void insertRecordIntoDbUserTable() throws SQLException {
-	 
-			Connection dbConnection = null;
-			Statement statement = null;
-	 
-			String insertTableSQL = "INSERT INTO DBUSER"
-					+ "( USERNAME, CREATED_BY, CREATED_DATE) " + "VALUES"
-					+ "('BILGE','SYSTEM', " + "to_date('"
-					+ getCurrentTimeStamp() + "', 'yyyy/mm/dd hh24:mi:ss'))";
-					
-				
-	 
-			try {
-				dbConnection = getDBConnection();
-				statement = dbConnection.createStatement();
-	 
-				System.out.println(insertTableSQL);
-				
-			 insert FILM; 
-			 values ('Forrest Gump',1994,'Drama, Romance','Tom Hanks', 'Rebecca Williams',142 , 'Forrest Gump life');
-				
-				 
-				
-				statement.executeUpdate(insertTableSQL);
-	 
-				System.out.println("Record is inserted into DBUSER table!");
-	 
-			} catch (SQLException e) {
-	 
-				System.out.println(e.getMessage());
-	 
-			} finally {
-	 
-				if (statement != null) {
-					statement.close();
-				}
-	 
-				if (dbConnection != null) {
-					dbConnection.close();
-				}
-	 
+	public static void main(String[] argv) {
+
+		try {
+
+			insertRecordIntoDbUserTable();
+			{
+				System.out.println("insert edildi");
 			}
-	 
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
 		}
-	 
-		private static Connection getDBConnection() {
-	 
-			Connection dbConnection = null;
-	 
-			try {
-	 
-				Class.forName(DB_DRIVER);
-	 
-			} catch (ClassNotFoundException e) {
-	 
-				System.out.println(e.getMessage());
-	 
-			}
-	 
-			try {
-	 
-				dbConnection = DriverManager.getConnection(
-	                               DB_CONNECTION, DB_USER,DB_PASSWORD);
-				return dbConnection;
-	 
-			} catch (SQLException e) {
-	 
-				System.out.println(e.getMessage());
-	 
-			}
-	 
-			return dbConnection;
-	 
-		}
-	 
-		private static String getCurrentTimeStamp() {
-	 
-			java.util.Date today = new java.util.Date();
-			return dateFormat.format(today.getTime());
-	 
-		
- 
+
 	}
-	
-	
+
+	private static void insertRecordIntoDbUserTable()
+			throws SQLException {
+
+		Connection dbConnection = null;
+		Statement statement = null;
+
+//		String insertTableSQL = "INSERT INTO DBUSER"
+//				+ "( USERNAME, CREATED_BY, CREATED_DATE) " + "VALUES"
+//				+ "('SYSTEM','SYSTEM', " + "to_date('" + getCurrentTimeStamp()
+//				+ "', 'yyyy/mm/dd hh24:mi:ss'))";
+
+		try {
+			dbConnection = getDBConnection();
+			statement = dbConnection.createStatement();
+
+			String insertTableSQL1 = "INSERT INTO FILM"
+					+ "(FILMADI,FILMYILI,FILMTURU,BASROLOYUNCULAR,FILMSURESI,FILMKONU)"
+					+ "VALUES"
+					+ "('Forrest Gump',1994,'Drama, Romance','Tom Hanks',142 , 'Forrest Gump life')";
+
+			System.out.println(insertTableSQL1);
+
+			// insert FILM;
+			// values ('Forrest Gump',1994,'Drama, Romance','Tom Hanks',
+			// 'Rebecca Williams',142 , 'Forrest Gump life');
+
+			statement.executeUpdate(insertTableSQL1);
+
+			System.out.println("Record is inserted into DBUSER table!");
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} finally {
+
+			if (statement != null) {
+				statement.close();
+			}
+
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
+
+		}
+
+	}
+
+	private static Connection getDBConnection() {
+
+		Connection dbConnection = null;
+
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println(e.getMessage());
+		}
+
+		try {
+
+			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,
+					DB_PASSWORD);
+			return dbConnection;
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+		return dbConnection;
+
+	}
+
+	private static String getCurrentTimeStamp() {
+
+		java.util.Date today = new java.util.Date();
+		return dateFormat.format(today.getTime());
+
+	}
+
 }
