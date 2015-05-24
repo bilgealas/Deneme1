e7cb93de87aaaf059265b97b3ef38282a880c1ee
bilgealas <bilgealas@gmail.com>
2015-05-24 22:24:40 +0300

    set islemi

--- OmdbTest/src/omdb/ResultSetExp.java
+++ OmdbTest/src/omdb/ResultSetExp.java
@@ -1,10 +1,14 @@
 package omdb;
 
+import java.awt.TextField;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class ResultSetExp {
 	
@@ -13,13 +17,9 @@ public class ResultSetExp {
 	private static final String DB_USER = "SYSTEM";
 	private static final String DB_PASSWORD = "bilge1510";
 	
-	
 	public static void main(String[] args) {
-		
-	
 		try {
-			
-		
+	
 			try {
 				Class.forName("oracle.jdbc.driver.OracleDriver");
 			} catch (ClassNotFoundException e) {
@@ -29,21 +29,35 @@ public class ResultSetExp {
 			
 			String connection ="jdbc:oracle:thin:@//localhost:1521/XE;DE_USER=SYSTEM;DB_PASSWORD=bilge1510";
 			Connection dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
-			
+		
 			Statement st = dbConnection.createStatement();
 			
-			String sql = "SELECT FILMADI, FILMYILI ,FILMTURU ,BASROLOYUNCULAR,FILMSURESI,FILMKONU FROM WHERE FILMADI = 'Vizontele'";
-			ResultSet sonuc= st.executeQuery("SELECT * FROM FILM WHERE FILMADI= 'Iron Man'");
+			Scanner scan=new Scanner(System.in);
+			System.out.println("Bir isim giriniz"); 
+            String  filmAdi=scan.nextLine();
+        //    String  filmTuru=scan.nextLine();
+            ExculatorServices  test = new ExculatorServices(filmAdi,null);
+            test.setFilmAdi(filmAdi);
+			//test.getFilmAdi();
+			//test.setFilmTuru(filmTuru);
+			//test.getfilmTuru();
 			
-			while(sonuc.next()){
-				System.out.println("Film adý :"+sonuc.getString("FILMADI")+" "
-			                      +"Film yýlý :"+sonuc.getInt("FILMYILI")+" "
-						          +"Film türü :"+sonuc.getString("FILMTURU")+" " 
-			                      +"Oyuncular :"+sonuc.getString("BASROLOYUNCULAR")+" "
-			                      +"Filmin süresi :"+sonuc.getInt("FILMSURESI") +" "
-			                      +"Konu :"+sonuc.getString("FILMKONU") );
-		}
+			String sql = "SELECT FILMADI, FILMYILI ,FILMTURU ,BASROLOYUNCULAR,FILMSURESI,FILMKONU FROM WHERE FILMADI";
 		
+			ResultSet sonuc= st.executeQuery("SELECT * FROM FILM WHERE FILMADI =\' filmAdi ==test.getFilmAdi() \' ");
+		
+		
+	//	if(sonuc.getString("FILMADI")==test.getFilmAdi()){
+			
+				while(sonuc.next()){
+					System.out.println("Film adý :"+sonuc.getString("FILMADI")+" , " 
+			                      +"Film yýlý :"+sonuc.getInt("FILMYILI")+" , "
+						          +"Film türü :"+sonuc.getString("FILMTURU")+" , " 
+			                      +"Oyuncular :"+sonuc.getString("BASROLOYUNCULAR")+" , "
+			                      +"Filmin süresi :"+sonuc.getInt("FILMSURESI") +" , "
+			                      +"Konu :"+sonuc.getString("FILMKONU") );
+	              }
+	//				} 
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
